0cc2ecc173299179178b56abf7e2b3d722aab8e7
Alex L <macbookpro@macbooks-mbp.home>
2020-03-17 17:39:05 +0000

    initial rspec

---
+++ spec/DockingStation_spec.rb
@@ -0,0 +1,9 @@
+#require "DockingStation"
+
+describe DockignStation do
+=begin
+  it "Is DockingStation defined as a class" do
+    expect(docking_station())# is a? instance of DockingStation
+  end
+=end
+end
