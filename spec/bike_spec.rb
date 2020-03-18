ac81b80b73cca35fc954c848a1fe79bf47d2e8f8
Faye Carter <fayecarter333@gmail.com>
2020-03-18 15:08:56 +0000

    Bike responds to working?

---
+++ spec/bike_spec.rb
@@ -0,0 +1,9 @@
+require './lib/bike'
+
+describe Bike do
+  context 'is #working? working?' do
+    it 'tests Bike responds to #working?' do
+      expect(Bike.new).to respond_to(:working?)
+    end
+  end
+end
