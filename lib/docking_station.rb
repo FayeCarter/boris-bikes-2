bd51b29b6ef1c278c90716dabea7059f032db4b1
ian-1 <ian.walton@portmoor.com>
2020-03-19 15:57:50 +0000

    refactored code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,25 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bike available" if @bikes == []
+    fail "No bike available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Dock full" if @bikes.length == 20
+    fail "Dock full" if full?
     @bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    return false if  @bikes.length < 20
+    true
+  end
+
+  def empty?
+    return false if  @bikes != []
+    true
   end
 
 end
